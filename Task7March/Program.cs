95c6c1dd15e520b888034bc08b67b151b7526a7f
PunhanAliyeva <eliyevapunhan567@gmail.com>
2024-03-07 10:58:37 +0400

    task is completed succesfully

---
+++ Task7March/Program.cs
@@ -0,0 +1,75 @@
+﻿
+
+
+//int[] arr1 = { 211, 9, -6, 12, 34, 56, 32, 5, 19 };
+//int[] arr2 = { 18, -6, 3 };
+//bool IsCheck = false;
+#region Method1
+//for (int i = 0; i < arr1.Length; i++)
+//{
+//    for (int j = 0; j < arr2.Length; j++)
+//    {
+//        if (arr1[i] == arr2[j])
+//        {
+//            IsCheck = true;
+//            break;
+//        }
+//        IsCheck = false;
+//    }
+//}
+//Console.WriteLine(IsCheck);
+#endregion
+
+
+#region Method2
+//foreach (int i in arr1)
+//{
+//    foreach (int j in arr2)
+//    {
+//        if(i == j)
+//        {
+//            IsCheck = true;
+//            break;
+//        }
+//        IsCheck = false;
+//    }
+//}
+//Console.WriteLine(IsCheck);
+#endregion
+
+
+#region Method3
+//foreach (int num in arr1)
+//{
+//	for (int j = 0; j < arr2.Length; j++)
+//	{
+//		if (arr2[j] == num)
+//		{
+//			IsCheck = true;
+//			break;
+//		}
+//	    IsCheck=false;
+//	}
+//}
+//Console.WriteLine(IsCheck);
+#endregion
+
+
+
+
+
+
+
+
+#region PyramidTask
+//int row = 5;
+//for (int i= 1; i <= row; i++)
+//{
+//    for (int j= 1; j <= i; j++)
+//    {
+//        Console.Write('*');
+//    }
+//    Console.WriteLine();
+//}
+#endregion
+
